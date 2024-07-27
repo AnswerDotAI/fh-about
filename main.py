23fa988a69b6f4cbb679f2274a09b1f9004569fa
Jeremy Howard <github@jhoward.fastmail.fm>
2024-07-27 19:52:37 +1000

    socials

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fh_bootstrap import *
 import vision, overview, foundations, tech, components
 
-hdrs = (Link(href='assets/hl-styles.css', rel='stylesheet'),)
+hdrs = (
+    Link(href='assets/hl-styles.css', rel='stylesheet'),
+    *Socials(title='FastHTML', description='Learn about FastHTML', site_name='about.fastht.ml',
+             twitter_site='@answerdotai', image=f'/assets/og-sq.png', url='')
+)
 
-app,rt = fast_app(pico=False, hdrs=bst_hdrs+hdrs, live=True)
+app,rt = fast_app(pico=False, hdrs=bst_hdrs+hdrs, live=False)
 
 @rt('/')
 def get(): return overview.page()
